f519bf113b556a1b0332a2d0ea29221db0e88980
Nghia Le <trongnghia203@gmail.com>
2020-12-02 17:11:17 +0700

    Package python project using setuptools with setup.py only

--- setup.py
+++ setup.py
@@ -1,6 +1,27 @@
 #!/usr/bin/env python3
-
+"""A setuptools based setup module.
+See:
+https://packaging.python.org/guides/distributing-packages-using-setuptools/
+https://github.com/pypa/sampleproject
+"""
 from setuptools import setup
 
-if __name__ == "__main__":
-    setup()
+setup(
+    name='simple-project',
+    version='1.1.0',  # Required
+    description='A simple project to practice the packaging with setuptools/setup.py',  # Optional
+    url='https://github.com/trongnghia203/simple_project',  # Optional
+    author='Nghia Le',  # Optional
+    author_email='nghia.le@gooddata.com',  # Optional
+    packages=["simple_code"],  # Required
+    python_requires='>=3.5, <4',  # Optional
+    install_requires=['pyyaml'],  # Optional
+    package_data={  # Optional
+        'simple_code': ['config.yaml'],
+    },
+    entry_points={  # Optional
+        'console_scripts': [
+            'simple.py=simple_code.simple:run',
+        ],
+    },
+)
